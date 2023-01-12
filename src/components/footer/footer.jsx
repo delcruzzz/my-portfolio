84062a1040928815083f22e64789098704f731de
Leonardo De la cruz <leonardodelacruz03@hotmail.com>
2023-01-11 22:50:50 -0500

    add essentials links 😅

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -4,6 +4,10 @@ export default function Footer() {
       <section className="flex flex-col items-center justify-center text-white py-8">
         <p className="">Made with ❤</p>
         <span>delcruz</span>
+        <section className="flex items-center justify-center space-x-16 py-4 text-3xl">
+          <a href="https://www.linkedin.com/in/leonardo-jos%C3%A9-de-la-cruz-ardila-08b65623b/" target="_blank">Linkedin</a>
+          <a href="https://twitter.com/delcruz88" target="_blank">Twitter</a>
+        </section>
       </section>
     </footer>
   )
--- src/components/navigation/res_navbar.jsx
+++ src/components/navigation/res_navbar.jsx
@@ -6,11 +6,13 @@ export default function ResNavbar({ toggle, handleMenu }) {
             <section className="flex items-center justify-end px-4 py-2">
                 <button onClick={ handleMenu }><Close /></button>
             </section>
-            <nav className="flex flex-col px-8 py-4 space-y-16 font-bold text-[2.8rem]">
+            <nav className="flex flex-col px-12 py-4 space-y-8 font-bold text-[2.8rem]">
                 <a href="#1" onClick={ handleMenu }>projects</a>
                 <a href="#2" onClick={ handleMenu }>technologies</a>
                 <a href="#" onClick={ handleMenu }>about me</a>
                 <a href="#" onClick={ handleMenu }>contact me</a>
+                <a href="https://www.linkedin.com/in/leonardo-jos%C3%A9-de-la-cruz-ardila-08b65623b/" target="_blank" onClick={ handleMenu }>linkedin</a>
+                <a href="https://twitter.com/delcruz88" target="_blank" onClick={ handleMenu }>twitter</a>
             </nav>
         </section>
     )
